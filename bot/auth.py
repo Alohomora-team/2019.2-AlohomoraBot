b295f3faff8c2540cb6f6a9f52f59d8bc454d6ef
luiscesm <luiscesm1@gmail.com>
2019-10-11 19:40:44 -0300

    Alterações na classe Auth

--- bot/auth.py
+++ bot/auth.py
@@ -5,8 +5,9 @@ import json
 import numpy
 import requests
 import subprocess
-
 import os
+
+from validator import ValidateAuth
 
 CPF_AUTH, VOICE_AUTH = range(2)
 
@@ -34,41 +35,10 @@ class Auth:
         cpf = update.message.text
         chat_id = update.message.chat_id
 
-        if(len(cpf) > 11 and cpf[3] == "." and cpf[7] == "." and cpf[11] == "-"):
-            cpf = cpf.replace('.','').replace('-','')
-
-        if(any(i.isalpha() for i in cpf) or "." in cpf or "-" in cpf or len(cpf) != 11):
-            update.message.reply_text('Por favor, digite o CPF com os 11 digitos: (Ex: 123.456.789-10)')
+        if not ValidateAuth.cpf(cpf, update):
             return CPF_AUTH
 
-        authCPF_J = (int(cpf[0])*10 +
-                    int(cpf[1])*9 +
-                    int(cpf[2])*8 +
-                    int(cpf[3])*7 +
-                    int(cpf[4])*6 +
-                    int(cpf[5])*5 +
-                    int(cpf[6])*4 +
-                    int(cpf[7])*3 +
-                    int(cpf[8])*2) % 11
-
-        authCPF_K = (int(cpf[0])*11 +
-                    int(cpf[1])*10 +
-                    int(cpf[2])*9 +
-                    int(cpf[3])*8 +
-                    int(cpf[4])*7 +
-                    int(cpf[5])*6 +
-                    int(cpf[6])*5 +
-                    int(cpf[7])*4 +
-                    int(cpf[8])*3 +
-                    int(cpf[9])*2) % 11
-
-        if((int(cpf[9]) != 0 and authCPF_J != 0 and authCPF_J != 1) and (int(cpf[9]) != (11 - authCPF_J))):
-            update.message.reply_text('CPF inválido, tente novamente:')
-            return CPF_AUTH
-
-        if((int(cpf[10]) != 0 and authCPF_K != 0 and authCPF_K != 1) and (int(cpf[10]) != (11 - authCPF_K))):
-            update.message.reply_text('CPF inválido, tente novamente:')
-            return CPF_AUTH
+        cpf = ValidateAuth.cpf(cpf, update)
 
         auth_chat[chat_id]['cpf'] = cpf
 
@@ -81,16 +51,8 @@ class Auth:
         chat_id = update.message.chat_id
         voice_auth = update.message.voice
 
-        if((voice_auth.duration)<1.0):
-            update.message.reply_text('Muito curto...O áudio deve ter 1 segundo de duração.')
-            update.message.reply_text('Por favor, grave novamente:')
+        if not ValidateAuth.voice(voice_auth, update):
             return VOICE_AUTH
-        elif((voice_auth.duration)>2.0):
-            update.message.reply_text('Muito grande...O áudio deve ter 2 segundo de duração.')
-            update.message.reply_text('Por favor, grave novamente:')
-            return VOICE_AUTH
-        else:
-            update.message.reply_text('Ótimo!')
 
         file_auth = voice_auth.get_file()
 
