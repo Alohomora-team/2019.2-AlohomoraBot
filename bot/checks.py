b8896c176d0ad9cc5dba726ef32ff1b2cad12f05
luiscesm <luiscesm1@gmail.com>
2019-10-15 13:08:00 -0300

    Adicionando logging de checks.
    Co-authored-by: Paulo Batista <matpaulo@hotmail.com.br>

--- bot/checks.py
+++ bot/checks.py
@@ -1,10 +1,14 @@
 from settings import PATH
 import os
 import requests
+import logging
+
+logger = logging.getLogger('Alohomora')
 
 class CheckCondo:
 
     def block(chat, chat_id):
+        logger.debug("Checking if the informed block exists in database")
         query = """
         query block($number: String!){
             block(number: $number){
@@ -18,10 +22,12 @@ class CheckCondo:
                 }
 
         response = requests.post(PATH, json={'query': query, 'variables':variables})
+        logger.debug(f"Response: {response.json()}")
 
         return response.json()
 
     def apartment(chat, chat_id):
+        logger.debug("Checking if the informed apartment exists in database")
         query = """
         query apartment($number: String!, $block: String!){
             apartment(number: $number, block: $block){
@@ -39,12 +45,14 @@ class CheckCondo:
                 }
 
         response = requests.post(PATH, json={'query': query, 'variables':variables})
+        logger.debug(f"Response: {response.json()}")
 
         return response.json()
 
 class CheckUser:
 
     def email(chat, chat_id):
+        logger.debug("Checking if the informed email exists in database")
         query = """
         query user($email: String!){
             user(email: $email){
@@ -58,10 +66,12 @@ class CheckUser:
                 }
 
         response = requests.post(PATH, json={'query': query, 'variables':variables})
+        logger.debug(f"Response: {response.json()}")
 
         return response.json()
 
     def cpf(chat, chat_id):
+        logger.debug("Checking if the informed CPF exists in database")
         query = """
         query user($cpf: String!){
             user(cpf: $cpf){
@@ -75,5 +85,6 @@ class CheckUser:
                 }
 
         response = requests.post(PATH, json={'query': query, 'variables':variables})
+        logger.debug(f"Response: {response.json()}")
 
         return response.json()
