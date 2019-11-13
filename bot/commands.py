85f56ec5d5c69d65ad6b1248ba5c1d8277fdb7c7
RodrigoTCLima <rodrigotiagocl@gmail.com>
2019-11-12 22:39:14 -0300

    Adição dos infos do logger.
    Co-authored-by: AlineLermen <AlineLermen@users.noreply.github.com>

--- bot/commands.py
+++ bot/commands.py
@@ -1,3 +1,4 @@
+from settings import LOG_NAME
 import logging
 
 logger = logging.getLogger(LOG_NAME)
@@ -6,7 +7,7 @@ chat = {}
 
 class Commands:
     def morador(update, context):
-        logger.info()
+        logger.info("Listing resident commands")
         update.message.reply_text("Como morador você pode:")
         update.message.reply_text("Autorizar a entrada de um visitante: /autorizar")
         update.message.reply_text("Cadastrar-se no sistema: /cadastrar")
@@ -14,7 +15,7 @@ class Commands:
         
 
     def visitante(update, context):
-        logger.info()
+        logger.info("Listing visitor commands")
         update.message.reply_text("Como visitante você pode:")
         update.message.reply_text("Realizar o pedido de uma visita: /visitar")
         update.message.reply_text("Dar um feedback ao serviço: /feedback")
