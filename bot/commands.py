c0ef0c1496d7b98373ebf2fc7bb4a87e368fd7d1
RodrigoTCLima <rodrigotiagocl@gmail.com>
2019-11-12 22:02:34 -0300

    Adição da do arquivo commands.py com as atuais possibilidades de comandos.
    Co-authored-by: AlineLermen <AlineLermen@users.noreply.github.com>

---
+++ bot/commands.py
@@ -0,0 +1,20 @@
+import logging
+
+logger = logging.getLogger(LOG_NAME)
+
+chat = {}
+
+class Commands:
+    def morador(update, context):
+        logger.info()
+        update.message.reply_text("Como morador você pode:")
+        update.message.reply_text("Autorizar a entrada de um visitante: /autorizar")
+        update.message.reply_text("Cadastrar-se no sistema: /cadastrar")
+        update.message.reply_text("Dar um feedback ao serviço: /feedback")
+        
+
+    def visitante(update, context):
+        logger.info()
+        update.message.reply_text("Como visitante você pode:")
+        update.message.reply_text("Realizar o pedido de uma visita: /visitar")
+        update.message.reply_text("Dar um feedback ao serviço: /feedback")
