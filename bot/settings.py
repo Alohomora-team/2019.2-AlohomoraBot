a551680404f2c000bcd2bb066354e127743f8486
João Luis Baraky <jlbaraky@gmail.com>
2019-11-23 22:43:32 -0300

    Arruma settings.py.
    Co-authored-by: Luis Furtado <luiscesm1@gmail.com>

--- bot/settings.py
+++ bot/settings.py
@@ -6,37 +6,19 @@ import logging
 # API Path
 PATH = "http://api:8000/graphql/"
 
-# Register constants
-NAME = 0
-PHONE = 1
-EMAIL = 2
-CPF = 3
-BLOCK = 4
-APARTMENT = 5
-PASSWORD = 6
-CATCH_AUDIO_SPEAKING_NAME = 7
-CONFIRM_AUDIO_SPEAKING_NAME = 8
-VOICE_REGISTER = 9
-REPEAT_VOICE = 10
+# Reisdent register constants
+(NAME,
+PHONE,
+EMAIL,
+CPF,
+BLOCK,
+APARTMENT,
+PASSWORD,
+VOICE_REGISTER,
+REPEAT_VOICE) = range(9)
 
 # Resident auth constants
-CPF_AUTH, VOICE_AUTH = range(2)
-
-#Visitor constants
-VISITOR_REGISTER_NAME = 0
-VISITOR_REGISTER_CPF = 1
-VERIFY_REGISTRATION = 2
-VISITOR_CPF = 3
-VISITOR_BLOCK = 4
-VISITOR_APARTMENT = 5
-CREATE_VISITOR_ENTRY = 6
-
-#Handle entry visitors constants
-HANDLE_VISITORS_PENDING = range(1)
-
-# Auth constants
-CHOOSE_AUTH, SHOW_VISITORS, CPF_AUTH, VOICE_AUTH, PASSWORD_AUTH = range(5)
->>>>>>> 6617d2015d8370f8f807b08eb1b9af349be81fa3
+CHOOSE_AUTH, VOICE_AUTH, PASSWORD_AUTH = range(3)
 
 # Visitor register constants
 VISITOR_REGISTER_NAME, VISITOR_REGISTER_CPF = range(2)
@@ -44,14 +26,14 @@ VISITOR_REGISTER_NAME, VISITOR_REGISTER_CPF = range(2)
 # Visit constants
 VISIT_BLOCK, VISIT_APARTMENT = range(2)
 
-# Admin auth constants
-ADMIN_AUTH_EMAIL, ADMIN_AUTH_PWD, ADMIN_AUTH_REPEAT = range(3)
-
 # Admin register constants
 ADMIN_REGISTER_EMAIL, ADMIN_REGISTER_PWD = range(2)
 
+# Admin auth constants
+ADMIN_AUTH_EMAIL, ADMIN_AUTH_PWD, ADMIN_AUTH_REPEAT = range(3)
+
 # Feedback constants
-FEEDBACK = range(1)
+FEEDBACK = 0
 
 # Log format
 FORMAT = '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
